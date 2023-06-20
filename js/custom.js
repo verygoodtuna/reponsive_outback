63d24d1ec92453b8138d6d60f68d569ef8448ba2
verygoodtuna <kjy990504@gmail.com>
2023-06-20 13:21:42 +0900

    Outback

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,39 @@
 $(function () {
-    
+    $(window).on('scroll', function() {
+        const sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('.Header').addClass('on')
+        }
+        else {
+        $('.Header').removeClass('on')
+        }
+    });
+
+    const MainSlide = new Swiper('.main_slide', {
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        on: {
+            slideChangeTransitionStart: function() {
+                $('.MainVisual .dots li')
+                .eq(this.eq)
+            }
+        }
+    });
+
+    $('.MainVisual .arrows .left').on('click', function () {
+        MainSlide.slidePrev();
+    })
+    $('.MainVisual .arrows .right').on('click', function () {
+        MainSlide.slideNext();
+    })
+
+    $('.MainVisual .dots li').on('click', function () {
+        const idx = $(this).index();
+        // 자기자신 번호 붙임
+        $(this).addClass('on').siblings().removeClass('on');
+        MainSlide.slideTo(idx);
+    })
 })
